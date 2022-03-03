a0fa0a59acbcca9a5f3139b09d55772783c2604d
Niji <medabots83@gmail.com>
2022-03-03 21:32:51 +1000

    Update and rename TestSammich.java to ch3_ex11_TestSammich.java

--- ch3_ex11_TestSammich.java
+++ ch3_ex11_TestSammich.java
@@ -1,7 +1,3 @@
-//Class name: TestSammich
-//Purpose: makes a sammich using set and get methods
-//Programmer: Judy Marie Delos Reyes on February 2, 2022
-
 import java.util.Scanner;
 
 public class TestSammich {
@@ -23,20 +19,10 @@ public class TestSammich {
 		price = keyboard.nextDouble();
 		
 		Sandwich object = new Sandwich(ingredient, bread, price);
-//		obj.setIngredient(ingredient);
-//		obj.setBread(bread);
-//		obj.setPrice(price);
 		
 		System.out.println("The ingredient used is " + ingredient + ".");
 		System.out.println("The bread used is " + bread + ".");
 		System.out.println("The price is $" + price + ".");
 		
-//		System.out.println("The ingredient used is " + obj.getIngredient());
-//		System.out.println("The bread used is " + obj.getBread());
-//		System.out.println("The price is $" + obj.getPrice());
-		
-//		System.out.println("Enter another price: ");
-//		double newPrice = keyboard.nextDouble();
-//		obj.setPrice(newPrice);
 	}
 }
